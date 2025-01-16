697b57d3192b55c7ade8519677a7cc2af9c7729d
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2025-01-16 20:04:21 +0530

    sorting

--- sorting/sort.cpp
+++ sorting/sort.cpp
@@ -2,13 +2,35 @@
 using namespace std;
 
 int main() {
-   vector <int> arr(5);
-   for(int i=0; i<arr.size(); i++){
-    cin>>arr[i];
-   }
-   sort(arr.begin(),arr.end());
-   for(int i : nums){
-    cout<<arr[i];
-   }
+//    vector <int> arr(5);
+//    for(int i=0; i<arr.size(); i++){
+//     cin>>arr[i];
+//    }
+//    sort(arr.begin(),arr.end()); // O(nlogn)
+//    for(int i : arr){
+//     cout<<i<<" ";
+//    }
+
+int arr[6] = {5,4,6,3,2,1};
+int n = 6;
+for(int i=0; i<n; i++){
+ cout<<arr[i]<<" ";
+}
+  for(int i=0; i<n-1; i++){ // n-1 passes
+  //traverse
+    for(int j=0; j<n-1-i; j++){ //swap
+    if(arr[j]>arr[j+1]) {
+        // int temp = arr[j];
+      // arr[j] = arr[j+1];
+      // arr[j+1] = temp;
+      swap(arr[j],arr[j+1]);
+    }
+  }
+} 
+cout<<endl;
+ for(int i=0; i<n; i++){
+ cout<<arr[i]<<" ";
+}
+
     return 0;
 }
