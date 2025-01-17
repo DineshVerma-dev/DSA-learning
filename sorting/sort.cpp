885074e98cfe20a195cf957df4f0f23ef326b37b
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2025-01-17 19:47:05 +0530

    buble sort end

--- sorting/sort.cpp
+++ sorting/sort.cpp
@@ -61,15 +61,57 @@ int main() {
 //   }
 
       //sort a string in decreasing order of value associated after removal of values smaller than x
-   sting s = "AZYZXBYDXJK";
-   string str = "";
-   for(int i=0; i<s.size(); i++){
-      if(s[i]>="X") {
-         str.push_back(s[i]);
-      }
-   }
-     sort(str.begin(),str.end());
-      cout<<str;
+   // string s = "AZYZXBYDXJK";
+   // string str = "";
+   // for(int i=0; i<s.size(); i++){
+   //    if(s[i]>='X') {
+   //       str.push_back(s[i]);
+   //    }
+   // }
+   //   sort(str.begin(),str.end());
+   //    cout<<str;
+
+   
+
+   // Push zeroes to end while maintaining the relative order of other elements
+   //  int arr[9]={5,0,1,2,0,0,4,0,3};
+   //  vector <int> res;
+   //  for(int i=0; i<9;i++){
+   //    if(arr[i] > 0){
+   //    res.push_back(arr[i]);
+   //    } 
+   //  }
+   //  for(int i=0; i<9; i++){
+   //      if(arr[i] == 0){
+   //          res.push_back(0);
+   //      }
+   //  }
+   // for(int i=0;i<res.size();i++){
+   //    cout<<res[i]<<" ";
+   // }
+  
+    
+int arr[]={5,0,1,2,0,0,4,0,3};
+int n = 9;
+for(int i=0; i<n; i++){
+ cout<<arr[i]<<" ";
+}
+  for(int i=0; i<n-1; i++){ // n-1 passes
+  //traverse
+    for(int j=0; j<n-1-i; j++){ //swap
+    if(arr[j]==0) {
+        // int temp = arr[j];
+      // arr[j] = arr[j+1];
+      // arr[j+1] = temp;
+      swap(arr[j],arr[j+1]);
+    }
+  }                // time complexity --> n(n-1)/2
+} 
+cout<<endl;
+ for(int i=0; i<n; i++){
+ cout<<arr[i]<<" ";
+}
+
 
     return 0;
 }
