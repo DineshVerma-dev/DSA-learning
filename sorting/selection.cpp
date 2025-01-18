986e6a4c0498c9fc877aa8875fd7baa857ed8679
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2025-01-18 15:27:02 +0530

    selection sort

---
+++ sorting/selection.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   
+  int arr[] = {5,3,1,4,2};
+  int minIndex = 0;
+  int n=4;
+  while(n){
+     for(int i=1; i<5;i++){
+     if(arr[minIndex] > arr[i]){
+        minIndex = i;
+     }
+      swap(arr[0], arr[minIndex]);
+     }
+     n--;
+  }
+  
+  for(int nums : arr){
+    cout<<nums<<" ";
+  }
+    return 0;
+}
