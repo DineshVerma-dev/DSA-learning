3594039124f14c8f29befd11bbeb7f2d5f5573fc
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2025-01-20 17:48:08 +0000

    solved two questions

--- sorting/question.cpp
+++ sorting/question.cpp
@@ -1,7 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-   
+// int majorityElement(vector<int>& nums) {
+//     int n = nums.size();
+//     for(int i=0; i<n; i++){
+//         int count = 1;
+//         for(int j=i+1; j<n; j++){
+//             if(nums[i] == nums[j]) count++;
+//         }
+//         if(count > (n/2) ) return nums[i];
+//     }
+//     return -1;
+// }   -------------------------> n2 time complexity
+
+//  int majorityElement(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         int n = nums.size();
+//         return nums[n/2];
+//     } --------------------------> nlogn time complexity
+
+// int main() {
+//     vector<int> nums = {2, 2, 1, 1, 1, 2, 2}; // Example input
+//     cout << "Majority Element: " << majorityElement(nums) << endl;
+//     return 0;
+// }
+
+
+// questions 19 12 23 8 16 array of element maintaing the order of arr give 3 2 5 0 1
+
+int main (){
+
+    int arr[] = {19,12,23,8,16};
+    int n = 5;
+    vector <int> v(n,0);
+    int x  = 0 ;
+    for(int i=0; i<n; i++){
+        int min = INT_MAX;
+        int mindx = -1;
+        for(int j=0; j<n ; j++){
+            if(v[j]==1) continue;
+            else {
+                if (min>arr[j]){
+                    min = arr[j];
+                    mindx = j;
+                }
+            }
+        }
+        arr[mindx] = x ;
+        v[mindx] = 1 ; //visited
+        x++;
+    }
+
+    for(int i=0; i<n ;i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
