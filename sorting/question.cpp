4ea4ffb8862a97bbe34fd579fde135fed4a1cdbb
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2025-01-21 14:21:47 +0000

    learning the Assign cookie question

--- sorting/question.cpp
+++ sorting/question.cpp
@@ -30,15 +30,40 @@ using namespace std;
 
 int main (){
 
+    // int arr[] = {19,12,23,8,16};
+    // int n = 5;
+    // vector <int> v(n,0);
+    // int x  = 0 ;
+    // for(int i=0; i<n; i++){
+    //     int min = INT_MAX;
+    //     int mindx = -1;
+    //     for(int j=0; j<n ; j++){
+    //         if(v[j]==1) continue;
+    //         else {
+    //             if (min>arr[j]){
+    //                 min = arr[j];
+    //                 mindx = j;
+    //             }
+    //         }
+    //     }
+    //     arr[mindx] = x ;
+    //     v[mindx] = 1 ; //visited
+    //     x++;
+    // }
+
+    // for(int i=0; i<n ;i++){
+    //     cout<<arr[i]<<" ";
+    // }
+ ///////////////////////////wthout using extra space
     int arr[] = {19,12,23,8,16};
     int n = 5;
-    vector <int> v(n,0);
+    
     int x  = 0 ;
     for(int i=0; i<n; i++){
         int min = INT_MAX;
         int mindx = -1;
         for(int j=0; j<n ; j++){
-            if(v[j]==1) continue;
+            if(arr[j]<=0) continue;
             else {
                 if (min>arr[j]){
                     min = arr[j];
@@ -46,8 +71,7 @@ int main (){
                 }
             }
         }
-        arr[mindx] = x ;
-        v[mindx] = 1 ; //visited
+        arr[mindx] = -x ;
         x++;
     }
 
