52aefaa89ac06519aa2d0a5323684a2f6eaab8cb
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2025-02-21 15:30:29 +0000

    almost done

--- recursion/Lect4.cpp
+++ recursion/Lect4.cpp
@@ -32,12 +32,61 @@ void generateBinary(string s,int n){
 }
 
 //leetcode 39 combintion sum
-void combination(){
+void combination(vector<int> v, int arr[],int n,int target,int idx){
+    if(target==0){
+        for(int i=0;i<v.size(); i++){
+             cout<<v[i]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    if(target<0) return ;
+    for(int i=idx; i<n; i++){
+        v.push_back(arr[i]);
+        combination(v,arr,n,target-arr[i],i);
+        v.pop_back();
+    }
+    
+}
+
+//leetcode 22 generate parenthesis
+void parenthesis(string s, int opening , int closing ,int n){
+    if(s.size() == 2*n){
+        //closing == n//
+       cout<<s<<" ";
+       cout<<endl;
+       return;
+    }  
+    if(opening < n) {
+        parenthesis(s+"(",opening+1,closing,n);
+    }
+    if(closing < opening){
+        parenthesis(s+")",opening,closing+1,n);
+    }
+}
+
+//leetcode  779 
+void grammer(string s, int n){
+    if(s.size() == n){
+        cout<<s<<" ";
+        cout<<endl;
+        return;
+    }
+    if(s.back() == '0'){
+        grammer(s+"01",n);
+    } else grammer(s+"10",n);
+   
     
 }
 int main(){
-     
-   
+ 
+    grammer("0",3);
+
+       //parenthesis("",0,0,3);
+        
+    //  int arr[] = {2,3,5};
+    //  vector<int> v;
+    // combination(v,arr,3,8,0);
 
   // generateBinary("",3);
 
--- recursion/tempCodeRunnerFile.cpp
+++ recursion/tempCodeRunnerFile.cpp
@@ -1,1 +1,16 @@
-cout<<x<<" ";
+void validcombination(vector<string> v, vector<string> arr, int n, int idx){
+    if(n == arr.size()){
+        for(int i=0;i<v.size(); i++){
+             cout<<v[i]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    
+    for(int i=idx; i<n; i++){
+        v.push_back(arr[i]);
+        validcombination(v,arr,n,i);
+        v.pop_back();
+    }
+    
+}
