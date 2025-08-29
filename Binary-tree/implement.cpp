64f36f68fe74af9f819c7e2c63c8a666f53f9175
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2025-08-29 06:52:42 +0000

    learning binary tree

---
+++ Binary-tree/implement.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node { //This is nothing but the binary tree
+ public :
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val = val;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+void displayTree(Node* root){
+    if(root == NULL) return;
+    cout<<root->val<<" ";
+  
+    displayTree(root->left);
+    displayTree(root->right);
+
+}
+
+int sum(Node* root){
+    if(root == NULL) return 0;
+   return root->val + sum(root->left) + sum(root->right);
+    
+}
+
+int sizeofBT(Node*root){
+    if(root == NULL) return 0;
+    return  1 + sizeofBT(root->left) + sizeofBT(root->right);
+}
+
+int findmax(Node* root){
+    if(root == NULL) return INT_MIN;
+    return max(root->val,max(findmax(root->left),findmax(root->right)));
+}
+
+int findlevel(Node* root){
+    if(root == NULL) return 0;
+    return 1 + max(findlevel(root->left) , findlevel(root->right));
+}
+int main(){
+    Node* a = new Node(1); // this is my root node
+    Node* b = new Node(2);
+    Node* c = new Node(3);
+    Node* d = new Node(4);
+    Node* e = new Node(5);
+    Node* f = new Node(6);
+    Node* g = new Node(7);
+
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left  = f;
+    c->right = g;
+   
+    displayTree(a);
+    cout<<sum(a)<<"sum";
+    cout<<endl;
+    cout<<sizeofBT(a)<<"SizeofBT";
+    cout<<endl;
+    cout<<findmax(a)<<"Findmax"<<endl;
+    cout<<findlevel(a)<<"FindLevel"<<endl;
+    return 0;
+}
--- stack/nextgreater.cpp
+++ stack/nextgreater.cpp
@@ -42,11 +42,10 @@ int main(){
      if(st.size()==0) ans[i] = -1;
      else ans[i] = st.top();
      st.push(i);
+     ans[i] = i - ans[i];
   }
  
-  for(int i=1; i<arr.size(); i++){
-      ans[i] = i - ans[i];
-  }
+  
   for(int i=0; i<ans.size(); i++){
     cout<<ans[i]<<" ";
   }
